89c3154b086d77f7d7dd1c18f49a56005676a8a2
asimha <asimha@qwinixtech.com>
2014-06-26 22:43:56 +0530

    Added styles for the buttons

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@ class QuestionsController < ApplicationController
 
 	def index
 		@questions = Question.all
+		@answers = @answers = Answer.where("question_id = ?", params[:question_id])
 	end
 
 	def new
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ QAndA::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => 'welcome#index'
+  root :to => 'questions#index'
   resources :questions
   resources :answers
   # post 
