1d90b32e48e5fd2a2aa509d6224e832a83db6779
asimha <asimha@qwinixtech.com>
2014-07-15 11:21:24 +0530

    smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 QAndA::Application.initialize!
+ config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :domain => "sendgrid.com",
+    :authentication => :login,
+    :user_name =>"akshaysimha",
+    :password => "simha1991"
+  }
+end
